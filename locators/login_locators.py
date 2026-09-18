e743d7c2e9395551899f641d1f13df9ba6e96f9f
I-D-D-I <3711080@gmail.com>
2026-09-19 02:49:08 +0300

    Add first Playwright test with Allure reporting

---
+++ locators/login_locators.py
@@ -0,0 +1,10 @@
+class LoginLocators:
+    """Локаторы для страницы логина"""
+    # Поля ввода
+    EMAIL_INPUT = "[data-qa-id='login_email_input']"
+    PASSWORD_INPUT = "[data-qa-id='login_password_input']"
+    # Кнопки
+    # LOGIN_BUTTON = "button[type='submit']"
+    LOGIN_BUTTON = "[data-qa-id='login_submit_button']"
+    # Регистрация
+    REGISTER_LINK = ("link", "Зарегистрироваться")
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,8 +1,19 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, expect  # Импортируем тип Page для аннотации и expect для проверок
+import allure
 
+@allure.title("Проверка открытия главной страницы Edversemovie")  # Добавляем название в отчет
+@allure.feature("Навигация")  # Группируем по функциональности
+def test_open_edversemovie(page: Page):  # page автоматически придет из фикстуры
+    """Проверяем, что открывается edversemovie."""
+    with allure.step("Открываем сайт"):  # Шаг для отчета
+        page.goto("https://edversemovie.ru/")
 
-def test_example(page: Page):
-    page.goto("https://example.com")
+    with allure.step("Проверяем заголовок"):  # Еще один шаг
+        expect(page).to_have_title("Cinescope")
 
-    expect(page).to_have_title("Example Domain")
-    expect(page.locator("h1")).to_have_text("Example Domain")
+    # Добавляем скриншот в отчет
+    allure.attach(
+        page.screenshot(),
+        name="Скриншот страницы",
+        attachment_type=allure.attachment_type.PNG
+    )
